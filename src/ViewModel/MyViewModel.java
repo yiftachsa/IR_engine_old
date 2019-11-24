2782e7b0a69186f1e6f328f5be6ad38571fe3c84
yiftachsa <yiftachs@post.bgu.ac.il>
2019-11-24 22:21:41 +0200

    Comments updates. View and viewModel package comments almost completed. Some comments are dependent on future decisions.

--- src/ViewModel/MyViewModel.java
+++ src/ViewModel/MyViewModel.java
@@ -28,10 +28,19 @@ public class MyViewModel extends Observable implements Observer {
         }
     }
 
+    /**
+     * Receives the "Stemming" checkbox state and forwards the selection to the model
+     * @param selected - boolean - the "Stemming" checkbox state
+     */
     public void stemmingSelection(boolean selected) {
         model.setStemming(selected);
     }
 
+    /**
+     * Receives the path of the dictionary from the user and forwards it to the model.
+     * @param path - String - the path of the dictionary
+     * @return - boolean - true if the loading completed successfully, else false
+     */
     public boolean loadDictionary(String path) {
         //TODO: load dictionary from path to object
         //TODO: check stemming!!!!
@@ -39,26 +48,42 @@ public class MyViewModel extends Observable implements Observer {
 
     }
 
+    /**
+     * Receives the path of the folder from which to clear all the files related
+     * to the IR engine and forwards it to the model.
+     * @param path - String - the path of the folder
+     * @return - boolean - true if deleting  the files completed successfully, else false
+     */
     public boolean clear(String path) {
         //TODO: clear the memory,posting,dictionary!
         //TODO"check if its already clear!
         return model.clear(path);
     }
 
+    /**
+     * Receives the dictionary status from the model.
+     * @return - boolean - true if the dictionary is loaded to the memory in the model, else false
+     */
     public boolean getDictionaryStatus() {
 
         return model.getDictionaryStatus();
     }
 
+    /**
+     *
+     * @return
+     */
     public String getDictionary() {
 
         return "";//(String)model.getDictionary();
     }
 
+    /**
+     * Forwards the text fields from the user to the model.
+     * @param corpusPath - String - "corpusPath" text field content
+     * @param resultPath - String - "resultPath" text field content
+     */
     public void start(String corpusPath, String resultPath) {
-
         model.start(corpusPath , resultPath);
-
-
     }
 }
