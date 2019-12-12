337006655e4f2c30da664d441a808e9da690bc9a
yiftachsa <yiftachs@post.bgu.ac.il>
2019-12-12 22:46:24 +0200

    Added function SaveSetEntities Need to check if the parser is going until the end!

--- src/main/java/CorpusProcessing/RunnableParse.java
+++ src/main/java/CorpusProcessing/RunnableParse.java
@@ -11,6 +11,7 @@ import java.util.concurrent.Future;
 
 public class RunnableParse implements Runnable {
 
+    private static final int MERGERSPOOLSIZE = 2;
     private HashSet<String> entities;
     private HashSet<String> singleAppearanceEntities;
     private File[] filesToParse;
@@ -36,16 +37,19 @@ public class RunnableParse implements Runnable {
 
     @Override
     public void run() {
-        ArrayList<Trio> entirePostingEntries = new ArrayList<>();
+        double startTime = System.currentTimeMillis()/1000;
+        String timePrint = "Thread: "+Thread.currentThread().getId()+" StartTime: "+startTime;
+
+
+        ArrayList<ArrayList<Trio>> entirePostingEntries = new ArrayList<>();
+        ExecutorService mergersPool = Executors.newFixedThreadPool(MERGERSPOOLSIZE); 
+        ArrayList<Future<ArrayList<Trio>>> futures = new ArrayList<>();
         for (File directory : filesToParse) {
             String filePath = directory.listFiles()[0].getAbsolutePath();
             if (Files.isReadable(Paths.get(filePath))) {
                 ArrayList<Document> documents = CorpusProcessing.ReadFile.separateFileToDocuments(filePath);
 
                 ArrayList<ArrayList<Trio>> postingEntriesListsOfFile = new ArrayList<>();
-
-                //ExecutorService mergersPool = Executors.newFixedThreadPool(4); //FIXME:MAGIC NUMBER
-                //ArrayList<Future<ArrayList<Trio>>> futures = new ArrayList<>();
 
                 for (Document document : documents) {
                     ArrayList<String> bagOfWords = parser.parseDocument(document);
@@ -56,37 +60,54 @@ public class RunnableParse implements Runnable {
                 while (postingEntriesListsOfFile.size() > 1) {
                     postingEntriesListsOfFile.add(Mapper.mergeAndSortTwoPostingEntriesLists(postingEntriesListsOfFile.remove(0), postingEntriesListsOfFile.remove(0)));
                 }
-                //merge postingEntriesListsOfFile into entirePostingEntries
-                entirePostingEntries=(Mapper.mergeAndSortTwoPostingEntriesLists(postingEntriesListsOfFile.get(0),entirePostingEntries));
-            }
-        }
-        Documenter.savePostingEntries(entirePostingEntries);
-                    /*
-                    if (allPostingEntriesLists.size() >= 2) {
-                        Future<ArrayList<Trio>> future = mergersPool.submit(new CallableMerge(allPostingEntriesLists));
-                        futures.add(future);
-                    }
-                    if (futures.size() > 0) {
-                        if (futures.get(0).isDone()) {
-                            Future<ArrayList<Trio>> future = futures.remove(0);
-                                try {
-                                    allPostingEntriesLists.add(future.get());
-                                } catch (Exception e) {
-                                    e.printStackTrace();
-                            }
+                //insert all posting entries of the file to entirePostingEntries
+                entirePostingEntries.add(postingEntriesListsOfFile.get(0));
+                //check if we can merge two posting list to one
+                if (entirePostingEntries.size() >= 2) {
+                    Future<ArrayList<Trio>> future = mergersPool.submit(new CallableMerge(entirePostingEntries.remove(0), entirePostingEntries.remove(0)));
+                    futures.add(future);
+                }
+                //Getting result from callableMerge
+                if (futures.size() > 0) {
+                    if (futures.get(0).isDone()) {
+                        Future<ArrayList<Trio>> future = futures.remove(0);
+                        try {
+                            entirePostingEntries.add(future.get());
+                        } catch (Exception e) {
+                            e.printStackTrace();
                         }
                     }
                 }
-                for (Future<ArrayList<Trio>> future : futures) {
-                            while (!future.isDone()) ;
-                            try {
-                                allPostingEntriesLists.add(future.get());
-                            } catch (Exception e) {
-                                e.printStackTrace();
-                    }
-                }
-                mergersPool.shutdown();
+                //merge postingEntriesListsOfFile into entirePostingEntries
 
-                */
+                //entirePostingEntries=(Mapper.mergeAndSortTwoPostingEntriesLists(postingEntriesListsOfFile.get(0),entirePostingEntries));
+
+            }
+        }
+        for (Future<ArrayList<Trio>> future : futures) {
+            while (!future.isDone()) ;
+            try {
+                entirePostingEntries.add(future.get());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        mergersPool.shutdown();
+        while (entirePostingEntries.size() > 1) {
+            entirePostingEntries.add(Mapper.mergeAndSortTwoPostingEntriesLists(entirePostingEntries.remove(0), entirePostingEntries.remove(0)));
+        }
+
+        double endTime = System.currentTimeMillis()/1000;
+        timePrint = timePrint+" EndTime: " +endTime + " Total: "+(endTime-startTime);
+        System.out.println(timePrint);
+
+        Documenter.savePostingEntries(entirePostingEntries);
+    }
+
+
+    public void saveAndClearEntitiesSets(){
+        Documenter.saveEntitiesSets(this.entities, this.singleAppearanceEntities);
+        this.entities = new HashSet<>();
+        this.singleAppearanceEntities = new HashSet<>();
     }
 }
