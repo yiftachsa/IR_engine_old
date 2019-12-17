f5126298c2857204a5dcf118807baee54d921d44
yiftachsa <yiftachs@post.bgu.ac.il>
2019-12-17 21:40:28 +0200

    GUI Fixed - Paths to FXML anf fxml files location changed to resources

--- src/main/java/View/GUI.java
+++ src/main/java/View/GUI.java
@@ -8,6 +8,10 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 
 public class GUI extends Application {
 
@@ -26,9 +30,15 @@ public class GUI extends Application {
         viewModel =  new MyViewModel(model);
         model.addObserver(viewModel);
 
-        primaryStage.setTitle("My Board Game");
-        FXMLLoader fxmlLoader = new FXMLLoader();
-        Parent root = fxmlLoader.load(getClass().getResource("GUI.fxml").openStream());
+//        Path p = Paths.get("./src/main/resources/GUI.fxml");
+//        System.out.println(p.toFile().getAbsolutePath());
+        primaryStage.setTitle("Information Retrieval Engine");
+        URL url = getClass().getResource("../GUI.fxml");
+        FXMLLoader fxmlLoader = new FXMLLoader();//(getClass().getResource("GUI.fxml"));
+
+        Parent root = fxmlLoader.load(url.openStream());
+
+
         Scene scene = new Scene(root , 400 , 330);
         primaryStage.setScene(scene);
         primaryStage.setMinHeight(330);
