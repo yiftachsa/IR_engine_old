c114f2c8624fcaad8fc97900e78df8ee20c5b54a
yiftachsa <yiftachs@post.bgu.ac.il>
2020-01-11 22:29:31 +0200

    Working version - 112 Relevant Documents. During Ranking optimization.

--- src/main/java/CorpusProcessing/Ranker.java
+++ src/main/java/CorpusProcessing/Ranker.java
@@ -23,7 +23,6 @@ public class Ranker implements IRanker {
     private static final double WEIGHT_HEADER = 0.05;
     private static final double WEIGHT_ENTITIES = 0.05;
 
-
     public Ranker(int corpusSize, double avdl, Indexer indexer) {
         this.corpusSize = corpusSize;
         this.avdl = avdl;
